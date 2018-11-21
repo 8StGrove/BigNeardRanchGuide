de788aa9918bf27519bd303c1152cd796c6e2c67
Haseeb Ahmed <haseeb_izah@msn.com>
2018-11-21 23:07:29 +0000

    Big Neard Ranch Guide

---
+++ app/src/main/java/geoquiz/andriod/bignerdranch/com/geoquiz/QuizActivity.java
@@ -0,0 +1,18 @@
+package geoquiz.andriod.bignerdranch.com.geoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
